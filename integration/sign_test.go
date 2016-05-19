45294050a8b2ba577ca02edb44675753da56a251
Mathew Koshy <matkoshy@in.ibm.com>
2016-05-19 17:26:58 +0530

    -sadded setuptest

--- integration/sign_test.go
+++ integration/sign_test.go
@@ -3,12 +3,16 @@ package main
 import (
 	"errors"
 	"github.com/go-check/check"
+	"os/exec"
+	"io/ioutil"
+	"os"
 	"io"
 	"strings"
 )
 
 const (
 	gpgBinary = "gpg"
+	manifestBinary = "manifest"
 )
 
 func init() {
@@ -48,4 +52,35 @@ func ConsumeAndLogOutput(c *check.C, id string, f io.ReadCloser, err error) {
 			}
 		}
 	}()
+}
+func (s *SigningSuite) SetUpTest(c *check.C) {
+	_, err := exec.LookPath(manifestBinary)
+	c.Assert(err, check.IsNil)
+	_, err = exec.LookPath(manifestBinary)
+	c.Assert(err, check.IsNil)
+
+	s.gpgHome, err = ioutil.TempDir("", "skopeo-gpg")
+	c.Assert(err, check.IsNil)
+	os.Setenv("GNUPGHOME", s.gpgHome)
+
+	cmd := exec.Command(gpgBinary, "--homedir", s.gpgHome, "--batch", "--gen-key")
+	stdin, err := cmd.StdinPipe()
+	c.Assert(err, check.IsNil)
+	stdout, err := cmd.StdoutPipe()
+	ConsumeAndLogOutput(c, "gen-key stdout", stdout, err)
+	stderr, err := cmd.StderrPipe()
+	ConsumeAndLogOutput(c, "gen-key stderr", stderr, err)
+	err = cmd.Start()
+	c.Assert(err, check.IsNil)
+	_, err = stdin.Write([]byte("Key-Type: RSA\nName-Real: Testing user\n%commit\n"))
+	c.Assert(err, check.IsNil)
+	err = stdin.Close()
+	c.Assert(err, check.IsNil)
+	err = cmd.Wait()
+	c.Assert(err, check.IsNil)
+
+	lines, err := exec.Command(gpgBinary, "--homedir", s.gpgHome, "--with-colons", "--no-permission-warning", "--fingerprint").Output()
+	c.Assert(err, check.IsNil)
+	s.fingerprint, err = findFingerprint(lines)
+	c.Assert(err, check.IsNil)
 }
